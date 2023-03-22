3b5bd5ec8079dddf0accec7c7f3042894413e959
Supun Kariyawasam <bhasura2105@gmail.com>
2023-03-22 11:53:00 +0530

    adjustments to LMSMainMenuConsumer

--- LMSMainMenuConsumer/src/lmsmainmenuconsumer/Activator.java
+++ LMSMainMenuConsumer/src/lmsmainmenuconsumer/Activator.java
@@ -1,9 +1,10 @@
 package lmsmainmenuconsumer;
 
+import adminsubscriber.SubscriberActivator;
 import assesmentsubscriber.AssmConsumerActivator;
 
 import java.util.Scanner;
-
+import lecturesubscriber.LecSubscriberActivator;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
@@ -14,6 +15,8 @@ public class Activator implements BundleActivator {
 	private static BundleContext context;
 	private ServiceReference assmRef;
 	private ServiceReference stdRef;
+	private ServiceReference lecRef;
+	private ServiceReference adminRef;
 
 	static BundleContext getContext() {
 		return context;
@@ -22,45 +25,55 @@ public class Activator implements BundleActivator {
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
 		
-		//connect to assessment consumer class
+		//connect to assessment consumer
 		assmRef = context.getServiceReference(AssmConsumerActivator.class.getName());
-		AssmConsumerActivator assesments = (AssmConsumerActivator) bundleContext.getService(assmRef);
+		AssmConsumerActivator assmMangement = (AssmConsumerActivator) bundleContext.getService(assmRef);
 		
-		//connect to student management consumer class
+		//connect to student management consumer
 		stdRef = context.getServiceReference(Subscriber_Activator.class.getName());
 		Subscriber_Activator stdManagement = (Subscriber_Activator) bundleContext.getService(stdRef);
+		
+		//connect to lecture Management consumer
+		lecRef = context.getServiceReference(LecSubscriberActivator.class.getName());
+		LecSubscriberActivator lecManagement = (LecSubscriberActivator) bundleContext.getService(lecRef);
+		
+		//connect to admin management consumer
+		adminRef = context.getServiceReference(SubscriberActivator.class.getName());
+		SubscriberActivator adminManagement = (SubscriberActivator) bundleContext.getService(adminRef);
+		
+		System.out.println("Main menu Loading.....");
 		
 		Scanner sc = new Scanner(System.in);
 		
 		while(true) {
 			
-			System.out.println("\n=================================");
-			System.out.println("Welcome to Pakaya LMS");
+			System.out.println("\n================================================");
+			System.out.println("\tWelcome to EduMart LMS");
 			int option = 0;
 			
-			System.out.println("\n=================================");
-			System.out.println("\n\n\nPlease select an option!\n");
+			System.out.println("================================================");
+			System.out.println("\n\nPlease select an option!\n");
 			System.out.println("1. Assessment Management");
 			System.out.println("2. Lecture Management ");
 			System.out.println("3. Student Management ");
 			System.out.println("4. Administrator");
-			System.out.println("5. Quit \n\n");
+			System.out.println("5. Quit \n");
 
 			System.out.print("Select an option :");
 			option = sc.nextInt();
 
 			switch (option) {
 			case 1:
-				assesments.displayMenu();
+				assmMangement.displayMenu();
 				break;
 			case 2:
-				System.out.println("Lecture Management");
+				lecManagement.displayMenu();
 				break;
 			case 3:
 				stdManagement.displayMenu();
 				break;
 			case 4:
-				System.out.println("Administrator Management");
+				adminManagement.displayMenu();
 			case 5:
 				break;
 
@@ -74,7 +87,7 @@ public class Activator implements BundleActivator {
 			
 		}
 		
-		System.out.println("Thank you for using Pakaya LMS");
+		System.out.println("Thank you for using EduMart LMS");
 		
 	}
 
